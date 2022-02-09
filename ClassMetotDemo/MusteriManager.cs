4adde34f62a17748d4451ea47c965a02a96087c6
yacnuzun <96738030+yacnuzun@users.noreply.github.com>
2022-02-10 00:44:49 +0300

    Add files via upload

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+    {
+        Musteri[] musteriler = new Musteri[] { };
+        public void Add(Musteri musteri,int musteriId,string musteriAd,string musteriSoyad)
+        {
+            musteri.Id = musteriId;
+            musteri.Ad = musteriAd;
+            musteri.Soyad = musteriSoyad;
+            musteriler=musteriler.Append(musteri).ToArray();
+            Console.WriteLine(musteriAd+" "+musteriSoyad+" Added!!");
+        }
+
+        public void View()
+        {
+            foreach (Musteri m in musteriler)
+            { 
+                Console.WriteLine(m.Id+ "\n" + m.Ad+"\n"+m.Soyad);
+                Console.WriteLine("------------------");
+            }
+            
+        }
+        public void Delete(int Id)
+        {
+            musteriler = musteriler.Where((source, index) => index != (Id-1)).ToArray();
+
+            Console.WriteLine((Id) + "id'li müşteri kaldırılmıştır.");
+
+        }
+    }
+}
