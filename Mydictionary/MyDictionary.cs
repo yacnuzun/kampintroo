2ef687f659113c918e91ee2bcd5b0147120e0cd1
yacnuzun <yacn.uzun@gmail.com>
2022-03-04 01:58:25 +0300

    04032022 01.58

---
+++ Mydictionary/MyDictionary.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mydictionary
+{
+    internal class MyDictionary<T, U>
+    {
+        T[] dictionaryKey;
+        U[] dictionaryValue;
+        T[] keyTempArray;
+        U[] valueTempArray;
+
+        public MyDictionary()
+        {
+            dictionaryKey = new T[0];
+            dictionaryValue = new U[0];
+        }
+        public void Add(T key, U value)
+        {
+            bool varmi = dictionaryKey.Contains(key);//Girilen anahtar değerini dizide arıyor..
+            if (varmi)
+            {
+                Console.WriteLine("Bu öge zaten var");
+            }
+            else
+            {
+                keyTempArray = dictionaryKey;//yedek diziye aktarılıyor..
+                dictionaryKey = new T[dictionaryKey.Length + 1];//dictionarykey eleman sayısı bir arttırılıyor.
+                for (int i = 0; i < dictionaryKey.Length; i++)//Keyleri eklemek için döngü..
+                {
+                    dictionaryKey[i] = keyTempArray[i];//kopya diziyi aktarıyor..
+                }
+                dictionaryKey[dictionaryKey.Length - 1] = key;//yeni girilen keyi ekliyor..
+
+                valueTempArray = dictionaryValue;//Değerleri yedekliyor..
+                dictionaryValue = new U[dictionaryValue.Length + 1];
+                for (int i = 0; i < dictionaryValue.Length; i++)//elemanalrı eklemek için döngü..
+                {
+                      dictionaryValue[i] = valueTempArray[i];//kopya diziyi aktarıyor..
+                }
+                dictionaryValue[dictionaryValue.Length - 1] = value;//girilen yeni değeri ekliyor..
+            }
+        }
+    }
+}
---
+++ Mydictionary/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mydictionary
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            MyDictionary<int,string> myDictionary = new MyDictionary<int, string>();
+            myDictionary.Add(1, "Ahmet");
+            myDictionary.Add(2, "Arzu");
+            Console.WriteLine(myDictionary.ToString());
+        }
+    }
+}
