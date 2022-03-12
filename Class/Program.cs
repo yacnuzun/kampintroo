9a95dba7d52e9a1157c1c8ba1a44fd9e62a1dd60
yacnuzun <yacn.uzun@gmail.com>
2022-03-12 15:04:03 +0300

    12322 154

---
+++ Class/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CustomerManager customerManager = new CustomerManager();
+            //customerManager.Add();
+            //customerManager.Update();
+            //Console.Read();
+            Customer customer = new Customer();
+            customer.Id = 1;
+            customer.City = "Ankara";
+            customer.FirstName = "Yalçın";
+            customer.LstName = "Uzun";
+            Customer customer2=new Customer() { Id = 2 ,City="İstanbul",FirstName="Miktad",LstName="Gürsel"};
+            Console.WriteLine(customer2.FirstName);
+            Console.Read();
+        }
+    }
+    class Customer
+    {
+        public int  Id { get; set; }
+        private string _firstName { get; set; }
+        public string FirstName {
+            get
+            {
+                return "Mr." + _firstName;
+            }
+            set {  _firstName=value; } }
+
+        public string LstName{ get; set; }
+
+        public string City{ get; set; }
+
+    }
+    class CustomerManager
+    {
+        public void Add()
+        {
+            Console.WriteLine("Customer Added!");
+        }
+        public void Update()
+        {
+            Console.WriteLine("Customer Update!!");
+        }
+    }
+}
---
+++ InterfacesDemo/Program.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfacesDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            IWorker[] workers = new IWorker[3]
+            {
+                new Manager(),
+            new Worker(),
+            new Robot()
+            };
+            foreach (var worker in workers)
+            {
+                Console.WriteLine(worker);
+            }
+            ISalary[] getSalarys = new ISalary[2]
+            {
+                new Manager(),
+                new Worker()
+            };
+            foreach(var getSalary in getSalarys)
+            {
+                Console.WriteLine(getSalary);
+            }
+            IEat[] eats = new IEat[2]
+            {
+                new Worker(),
+                new Manager()
+            };
+            foreach (var eat in eats)
+            {
+                Console.WriteLine(eat);
+            }
+        }
+    }
+    interface IWorker
+    {
+        void Work();
+
+    }
+    interface IEat
+    {
+
+        void Eat();
+
+    }
+    interface ISalary
+    {
+        void GetSalary();
+
+    }
+    class Worker : IWorker, ISalary, IEat
+    {
+        public void Eat()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void GetSalary()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Work()
+        {
+            throw new NotImplementedException();
+        }
+    }
+    class Manager : IWorker, ISalary, IEat
+    {
+        public void Eat()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void GetSalary()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Work()
+        {
+            throw new NotImplementedException();
+        }
+    }
+    class Robot : IWorker
+    {
+        public void Work()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
